c277a04273eefe524116cc1dd921f79ecd65f41d
Momchil Minkov <momcil.minkov@softwareone.com>
2023-09-18 14:29:07 +0300

    Edit of the locals

--- _locals.tf
+++ _locals.tf
@@ -1,24 +1,19 @@
 locals {
-  name            = "swo-containers-group"
-  cluster_version = "1.27"
-  region          = var.region
+  vpc_name   = "containers-group-vpc"
+  region     = var.region
+  account_id = var.account_id == null ? data.aws_caller_identity.current.account_id : var.account_id
+  # partition  = data.aws_partition.current.partition
 
   tags = merge({
-    Example    = local.name
+    Example    = local.cluster_name
     GithubRepo = "terraform-aws-eks"
     GithubOrg  = "terraform-aws-modules"
   }, var.tags)
 }
 
-### Logging module locals:
 locals {
-  partition  = data.aws_partition.current.partition
-  account_id = var.account_id == null ? data.aws_caller_identity.current.account_id : var.account_id
-  #cluster_name          = var.fluentbit_cluster_info_configs["cluster.name"]
-  eks_oidc_issuer_url   = replace(data.aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer, "https://", "")
-  eks_oidc_provider_arn = "arn:${local.partition}:iam::${local.account_id}:oidc-provider/${local.eks_oidc_issuer_url}"
-}
-
-locals {
-  cluster_name = "green-belt-eks-cluster"
+  cluster_name    = "green-belt-eks-cluster"
+  cluster_version = "1.27"
+  # eks_oidc_issuer_url   = replace(data.aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer, "https://", "")
+  # eks_oidc_provider_arn = "arn:${local.partition}:iam::${local.account_id}:oidc-provider/${local.eks_oidc_issuer_url}"
 }
