cf5e98167b0f9e1519c0ece76cb2f488bc3adb0a
Momchil Minkov <momcil.minkov@softwareone.com>
2023-10-26 15:40:59 +0300

    ArgoCD ApplicationSet deployment

--- argocd-config.tf
+++ argocd-config.tf
@@ -1,4 +1,4 @@
-########################## ArgoCD repository ##########################
+########################## ArgoCD repository ##############################
 resource "local_file" "argocd_repository" {
   filename = "templates/argocd-repo.yaml"
   content  = file("${path.module}/templates/argocd-repo.yaml")
@@ -15,19 +15,37 @@ resource "null_resource" "argocd_repository" {
   depends_on = [module.eks]
 }
 
-########################## ArgoCD applications ##########################
-resource "local_file" "argocd_app_game" {
-  filename = "templates/argocd-apps.yaml"
-  content  = file("${path.module}/templates/argocd-apps.yaml")
+########################### ArgoCD Application Set ##########################
+resource "local_file" "argocd_appset" {
+  filename = "templates/argocd-appset.yaml"
+  content  = file("${path.module}/templates/argocd-appset.yaml")
 }
 
-resource "null_resource" "argocd_app_game" {
+resource "null_resource" "argocd_appset" {
   provisioner "local-exec" {
     command = <<EOT
       aws eks update-kubeconfig --region ${local.region} --name ${local.cluster_name} ;
-      kubectl apply -f ${local_file.argocd_app_game.filename}
+      kubectl apply -f ${local_file.argocd_appset.filename}
       EOT
   }
 
   depends_on = [module.eks]
 }
+
+########################### ArgoCD application ##############################
+
+### Uncomment if you want to deploy only the 2048 game.
+# resource "local_file" "argocd_app_game" {
+#   filename = "templates/argocd-apps.yaml"
+#   content  = file("${path.module}/templates/argocd-apps.yaml")
+# }
+
+# resource "null_resource" "argocd_app_game" {
+#   provisioner "local-exec" {
+#     command = <<EOT
+#       aws eks update-kubeconfig --region ${local.region} --name ${local.cluster_name} ;
+#       kubectl apply -f ${local_file.argocd_app_game.filename}
+#       EOT
+#   }
+
+#   depends_on = [module.eks]
