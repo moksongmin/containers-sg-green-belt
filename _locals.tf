2dad9213b1f9b6c70a49672171169059edf85312
Momchil Minkov <momcil.minkov@softwareone.com>
2024-01-23 20:31:56 +0200

    Upgrade EKS cluster to the latest version

--- _locals.tf
+++ _locals.tf
@@ -11,7 +11,7 @@ locals {
 
 locals {
   cluster_name    = "gbcluster"
-  cluster_version = "1.27"
+  cluster_version = "1.28"
   # eks_oidc_issuer_url   = replace(data.aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer, "https://", "")
   # eks_oidc_provider_arn = "arn:${local.partition}:iam::${local.account_id}:oidc-provider/${local.eks_oidc_issuer_url}"
 }
